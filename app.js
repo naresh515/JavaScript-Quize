77c2e9875b5ad75160f6c227a9848bea244f6f20
Naresh Tak <taknaresh515@gmail.com>
2023-06-02 18:15:10 +0530

    change some js

--- app.js
+++ app.js
@@ -215,8 +215,8 @@ function showResult() {
                 '<input type="radio" disabled>' +
                 answer.option;
 
-            const massageElement = document.createElement('span');
-            massageElement.innerHTML = `You Didn't Attempt this Question`;
+            // const massageElement = document.createElement('span');
+            // massageElement.innerHTML = `You Didn't Attempt this Question`;
             if (correctAnswer) {
                 if (userSelect === true && answer.answer === true) {
                     answerElement.style.color = "green";
@@ -226,13 +226,11 @@ function showResult() {
                 } else if (answer.answer === true) {
                     answerElement.style.color = "green";
 
-                } else if (userSelect === false) {
+                } else if (userSelect === false && answer.answer === false) {
                     answerElement.style.color = "red";
-                    if (userSelect === true && answer.answer === false) {
-                        answerElement.innerHTML =
-                            '<input type="radio" checked>' +
-                            answer.option;
-                    }
+                    answerElement.innerHTML =
+                        '<input type="radio" checked>' +
+                        answer.option;
                 } else {
                     answerElement.style.color = "red";
                 }
